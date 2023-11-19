6ac471a4c1cdaa4f4b1e38aea7fa56a0769a8f9a
andBabaev <babaev-andrei2011@yandex.ru>
2023-11-19 14:31:12 +0300

    simple page

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Student Stories",
+    page_icon="🤗",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+    menu_items={}
+)
+
 st.title('#твоеслово.фтт')
 
-txt_input = st.text_area(label='Расскажи что-нибудь', placeholder='Нажми и печатай')
+with st.form('form'):
+    st.markdown('Расскажи что-нибудь')
+    txt_input = st.text_area(label='.', placeholder='Нажми и печатай', 
+                             label_visibility='collapsed')
+    submitted = st.form_submit_button("Поделиться")
+    if submitted and txt_input:
+        st.success('Отправлено!')
+    elif submitted and not txt_input:
+        st.warning('Напишите чего-нибудь')
+    
