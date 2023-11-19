11d74decc91fc241d8b4020d5bdfa92ad9497bb9
andBabaev <babaev-andrei2011@yandex.ru>
2023-11-19 15:15:45 +0300

    hide footer

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ st.set_page_config(
 hide_decoration_bar_style = '''
     <style>
         header {visibility: hidden;}
+        footer {visibility: hidden;}
     </style>
 '''
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
